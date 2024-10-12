cd0b4d5890af69b43a663f41ccaacd20a7834fa4
yanchies <yancheng.chu@gmail.com>
2024-10-12 15:57:36 +0800

    document query text

--- logics/rag.py
+++ logics/rag.py
@@ -24,26 +24,24 @@ def init_split(json_file_path):
 embeddings_model = OpenAIEmbeddings(model='text-embedding-3-small')
 
 def create_vector_store(file):
-    # Initialize the vector store only if it's not already in session_state
-    if 'vector_store' not in st.session_state:
-        # Split the documents
-        documents = init_split(file)
-        
-        # Create the vector store from documents
-        vector_store = Chroma.from_documents(
-            collection_name="pictsense_store",
-            documents=documents,
-            embedding=embeddings_model,
-            persist_directory="./chroma_langchain_db"  # Local persistence
-        )
-        
-        # Store the vector store in session_state
-        st.session_state['vector_store'] = vector_store
-        st.write("Vector store created and stored in session state.")
-    else:
-        # Retrieve from session_state if it already exists
-        vector_store = st.session_state['vector_store']
-        st.write("Vector store loaded from session state.")
+    # Split the documents
+    documents = init_split(file)
+    
+    # Create the vector store from documents
+    vector_store = Chroma.from_documents(
+        collection_name="pictsense_store",
+        documents=documents,
+        embedding=embeddings_model,
+        persist_directory="./chroma_langchain_db"  # Local persistence
+    )
+    
+    # Store the vector store in session_state
+    st.session_state['vector_store'] = vector_store
+    st.write("Vector store created and stored in session state.")
+    # else:
+    #     # Retrieve from session_state if it already exists
+    #     vector_store = st.session_state['vector_store']
+    #     st.write("Vector store loaded from session state.")
     
     return vector_store
 
