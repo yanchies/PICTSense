57656c78ae6ffc0fed80658fd04c9242ca9ef8e5
yanchies <yancheng.chu@gmail.com>
2024-10-12 16:14:28 +0800

    delete old chroma db

--- logics/rag.py
+++ logics/rag.py
@@ -7,8 +7,9 @@ from langchain_chroma import Chroma
 from langchain.chains import RetrievalQA
 import json
 import streamlit as st
+import shutil
 
-splitter = RecursiveJsonSplitter(max_chunk_size=200)
+splitter = RecursiveJsonSplitter(max_chunk_size=500)
 
 def init_split(json_file_path):
     """Initialize document splitting from JSON file."""
@@ -23,28 +24,26 @@ def init_split(json_file_path):
 
 embeddings_model = OpenAIEmbeddings(model='text-embedding-3-small')
 
-def create_vector_store(file):
-    # Split the documents
+def create_vector_store(file, force_create=False):
     documents = init_split(file)
+
+    # to delete the old vector store
+    if db.check_persist_directory("./chroma_langchain_db"):
+        shutil.rmtree("./chroma_langchain_db")
     
-    # Create the vector store from documents
+    # Create new vector store from documents
+    st.write("Creating new vector store...")
     vector_store = Chroma.from_documents(
         collection_name="pictsense_store",
         documents=documents,
         embedding=embeddings_model,
         persist_directory="./chroma_langchain_db"  # Local persistence
     )
-    
+    st.write("Vector store created.")
+  
     # Store the vector store in session_state
     st.session_state['vector_store'] = vector_store
-    st.write("Vector store created and stored in session state.")
-    # else:
-    #     # Retrieve from session_state if it already exists
-    #     vector_store = st.session_state['vector_store']
-    #     st.write("Vector store loaded from session state.")
-    
     return vector_store
-
 
 # load the vector store
 db = Chroma("pictsense_store",
