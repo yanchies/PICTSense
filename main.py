fdd869625b386cdba64e7cec12478b5b71e908f8
yanchies <yancheng.chu@gmail.com>
2024-10-12 15:51:45 +0800

    adjusted main

--- main.py
+++ main.py
@@ -19,30 +19,17 @@ def main():
     
     if 'file' not in st.session_state:
         file = st.file_uploader(label="Upload a .csv file", type=['csv'])
-        st.success("File uploaded successfully!")
-        st.session_state['file'] = json_file_path
-        st.divider()
-
-    if file is not None:
-        csv_file_path, json_file_path = file_uploader(file)
-        df = pd.read_csv(csv_file_path)  # Load your CSV file
-        json_file_path = process_responses(df, json_file_path)
-
-        st.subheader("Dataframe")
-        st.dataframe(get_df(json_file_path))        
-
-        # result = run_crew(file_path)
-        # #st.subheader("Task Output 1")
-        # #st.write(result.tasks_output[0].raw)
-        # #st.write(result.tasks_output[0])
-        # st.subheader("Task Output 2")
-        # st.write(result.tasks_output[1].raw)
-        # st.write(result.tasks_output[1])
-        # st.subheader("Task Output 3")
-        # st.write(result.tasks_output[2].raw)
-        # st.write(result.tasks_output[2])
-        # st.subheader("Task Output 4")
-        # st.write(result.tasks_output[3].raw)
+        
+        if file is not None:
+            csv_file_path, json_file_path = file_uploader(file)
+            st.success("File uploaded successfully!")
+            st.session_state['file'] = json_file_path
+            st.divider()
+            
+            st.subheader("Dataframe")
+            df = pd.read_csv(csv_file_path)  # Load your CSV file
+            json_file_path = process_responses(df, json_file_path)
+            st.dataframe(get_df(json_file_path))        
     
     else:
         # Message to show if no file is uploaded
