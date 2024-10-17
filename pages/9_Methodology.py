73ccdd145bef2ea0b4eb002b7f38bd6bc76e9740
yanchies <yancheng.chu@gmail.com>
2024-10-17 16:03:22 +0800

    writeup

--- pages/9_Methodology.py
+++ pages/9_Methodology.py
@@ -2,10 +2,10 @@ import streamlit as st
 st.title('Methodology')
 st.write("""
          The application contains two key use cases: (1) Sentiment Analysis & Topic Identification; and (2) Response Query.
-         See below for the process flows of the two use cases.
-         In summary, the application utilises various LLM-based techniques such as Prompt Engineering, use of embeddings, 
-         Retrieval-Augment Generation (RAG), and RetrievalQA to enable its features.   
+         See below for the process flows of the two use cases.   
          """)
+st.write("""In summary, the application utilises various LLM-based techniques such as Prompt Engineering, use of embeddings, 
+         Retrieval-Augment Generation (RAG), and RetrievalQA to enable its features.""")
 
 st.subheader("Process Flow for Sentiment Analysis & Topic Identification")
 # pass in csv file
