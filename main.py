458b644fdb4a56114a1df430a856a4803884cea5
yanchies <yancheng.chu@gmail.com>
2024-10-17 15:29:18 +0800

    notice

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ def main():
 
     st.title("PICTSense - Analytical Tool for Open-Ended Responses")
 
-    st.expander("""
-    IMPORTANT NOTICE: This web application is developed as a proof-of-concept prototype. The information provided here is NOT intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+    with st.expander("IMPORTANT NOTICE"):
+        st.write("""
+    This web application is developed as a proof-of-concept prototype. The information provided here is NOT intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
     Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
     Always consult with qualified professionals for accurate and personalized advice.
     """)
