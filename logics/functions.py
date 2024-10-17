511d6b028be082e51033482bf81eefe9d4747a90
yanchies <yancheng.chu@gmail.com>
2024-10-17 14:19:01 +0800

    str

--- logics/functions.py
+++ logics/functions.py
@@ -20,8 +20,8 @@ def analyze_sentiment_batch(responses):
         message = {
             "role": "user", 
             "content": f"You must only output a numerical sentiment score (example, 2), from a scale of 1 to 10 \
-                with 1 being most negative and 10 being most positive, for the following survey response: {response}.
-                A score of 1 would refer to responses that exhibit extreme dissatisfaction and negativity \
+                with 1 being most negative and 10 being most positive, for the following survey response: {response}.\
+                    A score of 1 would refer to responses that exhibit extreme dissatisfaction and negativity \
                     from language, content, and tone (punctuation as well). 10 would refer to extreme positivity \
                         and satisfaction."
         }
